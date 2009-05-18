95952161ebbd20598edad40f37e5beb1a56f5298
Martin Kleppmann <martin@eptcomputing.com>
2009-05-18 17:28:14 +0100

    Added comments

--- src/main/java/com/example/model/Predicates.java
+++ src/main/java/com/example/model/Predicates.java
@@ -2,7 +2,12 @@ package com.example.model;
 
 import org.neo4j.api.core.RelationshipType;
 
+// Scala can't produce Java-compatible enums, so they will have to be
+// defined in Java for now.
 
+/**
+ * Enum of all the possible relationships between nodes known at compile-time.
+ */
 public enum Predicates implements RelationshipType {
     KNOWS
 }
