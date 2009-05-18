7c59c00d8438da58d46c78ed6acb13bb22aee862
Martin Kleppmann <martin@eptcomputing.com>
2009-05-18 16:59:16 +0100

    Startup with external config + clean shutdown

--- src/main/scala/com/example/InitServlet.scala
+++ src/main/scala/com/example/InitServlet.scala
@@ -1,13 +1,33 @@
 package com.example
 
+import java.io.{InputStreamReader, IOException}
+import java.util.Properties
 import java.util.logging.Logger
 import javax.servlet.http.HttpServlet
 
+/**
+ * This servlet is automatically initialized when the web app is started.
+ * Here we set up the Neo4j server for the rest of the application to use.
+ */
 class InitServlet extends HttpServlet {
   val log = Logger.getLogger(this.getClass.getName) 
 
   override def init {
     log.info("Initializing Neo4J server.")
-    // Do initialization
+    // Load Neo4j configuration from properties file. You may want to replace this with something
+    // more clever, e.g. loading different configs for development/staging/production environments.
+    val neoConfig = new Properties
+    try {
+      val config = getServletContext.getResourceAsStream("/WEB-INF/neo4j.properties")
+      neoConfig.load(new InputStreamReader(config))
+    } catch {
+      case e: IOException => log.info("Cannot read Neo4j configuration: " + e)
+    }
+    NeoServer.startup(neoConfig)
+  }
+
+  override def destroy {
+    log.info("Shutting down Neo4J server.")
+    NeoServer.shutdown
   }
 }
--- src/main/scala/com/example/NeoServer.scala
+++ src/main/scala/com/example/NeoServer.scala
@@ -1,20 +1,43 @@
 package com.example
 
+import java.util.Properties
 import org.neo4j.api.core.EmbeddedNeo
 
+/**
+ * Wrapper around a singleton instance of Neo4j embedded server.
+ */
 object NeoServer {
-  val neo = new EmbeddedNeo("/tmp/neo4j")
-  
-  def exec[T<:AnyRef](operation: EmbeddedNeo => T): T = {
-    val tx = neo.beginTx()
-    try {
-      val ret = operation(neo)
-      tx.success()
-      return ret
-    } finally {
-      tx.finish();
-    }
+
+  private var neo: EmbeddedNeo = null
+
+  /**
+   * Initialize Neo4j with configuration from a properties file.
+   */
+  def startup(config: Properties) {
+    if (neo != null) neo.shutdown
+    neo = new EmbeddedNeo(config.getProperty("path", "/tmp/neo4j"))
   }
 
-  // somehow catch shutdown and run: neo.shutdown
+  /**
+   * Do a clean shutdown of Neo4j.
+   */
+  def shutdown {
+    if (neo != null) neo.shutdown
+    neo = null
+  }
+
+  /**
+   * Execute instructions within a Neo4j transaction; rollback if exception is raised and
+   * commit otherwise; and return the return value from the operation.
+   */
+  def exec[T<:AnyRef](operation: EmbeddedNeo => T): T = {
+    val tx = neo.beginTx
+    try {
+      val ret = operation(neo)
+      tx.success
+      return ret
+    } finally {
+      tx.finish
+    }
+  }
 }
