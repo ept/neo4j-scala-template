64d22ba1869d27e109030a302e218eb8af1f0a1d
Martin Kleppmann <martin@eptcomputing.com>
2009-06-14 18:00:18 +0100

    Rename ExampleNeoResource to NeoResource

--- src/main/scala/com/example/restapi/ExampleNeoResource.scala
+++
@@ -1,7 +0,0 @@
-package com.example.restapi
-
-import javax.ws.rs._
-import com.eptcomputing.neo4j.rest.NeoResource
-
-@Path("/neo_resource")
-class ExampleNeoResource extends NeoResource
---
+++ src/main/scala/com/example/restapi/NeoResource.scala
@@ -0,0 +1,7 @@
+package com.example.restapi
+
+import javax.ws.rs.Path
+import com.eptcomputing.neo4j.rest.SimpleNeoResource
+
+@Path("/neo_resource")
+class NeoResource extends SimpleNeoResource
